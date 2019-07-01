8c16cd33667932e7d2c191e7d5ecf9bd74f07b4e
Dalton <phandb72@gmail.com>
2019-06-30 23:53:45 -0500

    Update project packages

--- RESTfulPlayerApplication/App_Start/BundleConfig.cs
+++ RESTfulPlayerApplication/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace RESTfulPlayerApplication
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-cyborg.css",
                       "~/Content/site.css"));
         }
     }
